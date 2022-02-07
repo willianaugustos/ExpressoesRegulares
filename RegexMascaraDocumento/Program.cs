1cfc86c369eab24ab96b3389e3f41537025b62d7
Willian <willian@bytebr.com.br>
2022-02-07 20:35:06 -0300

    Adicionando projeto de exemplo de mascaramento do CPF

---
+++ RegexMascaraDocumento/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+var cpf = "123.456.789-00";
+Console.WriteLine($"Documento sem a máscara: {cpf}");
+
+var regex = new Regex(@"^(\d{2,3})\.\d{3}\.\d{3}\-(\d{2})");
+var cpfMask = regex.Replace(cpf, "$1.***.***.$2");
+
+Console.WriteLine($"Documento com a máscara: {cpfMask}");
