14d447c40bbebdca717e90c6a765b2492173dc65
Willian <willian@bytebr.com.br>
2022-02-08 06:15:01 -0300

    Correção na expressão replace, trocando . por -

--- RegexMascaraDocumento/Program.cs
+++ RegexMascaraDocumento/Program.cs
@@ -4,6 +4,6 @@ var cpf = "123.456.789-00";
 Console.WriteLine($"Documento sem a máscara: {cpf}");
 
 var regex = new Regex(@"^(\d{2,3})\.\d{3}\.\d{3}\-(\d{2})");
-var cpfMask = regex.Replace(cpf, "$1.***.***.$2");
+var cpfMask = regex.Replace(cpf, "$1.***.***-$2");
 
 Console.WriteLine($"Documento com a máscara: {cpfMask}");
